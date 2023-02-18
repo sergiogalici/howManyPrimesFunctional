3eea44b180140757390af6a10dd8d64643de4458
sergiogalici <103759691+sergiogalici@users.noreply.github.com>
2023-02-18 06:54:12 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         List<Integer> nums = IntStream
-                .range(2, 101)
+                .range(1, 101)
                 .boxed()
                 .collect(Collectors.toList());
 
